25486aba4447ae5207daa46b1e9d1e8684ebaf81
Md Maaz Ahmad <nehalmaaz@gmail.com>
2025-02-06 06:02:44 -0800

    customDelimiterSuccessScreenShot

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -4,7 +4,15 @@ class StringCalculator {
       return 0;
     }
 
-    final parts = numbers.replaceAll('\n', ',').split(',');
+    String delimiter = ',';
+    if (numbers.startsWith("//")) {
+      int delimiterEnd = numbers.indexOf('\n');
+      delimiter = numbers.substring(2, delimiterEnd);
+      numbers = numbers.substring(delimiterEnd + 1);
+    }
+
+    List<String> parts = numbers.replaceAll('\n', delimiter).split(delimiter);
+
     return parts.map(int.parse).reduce((a, b) => a + b);
   }
 }
